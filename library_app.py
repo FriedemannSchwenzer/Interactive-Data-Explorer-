134784dfa0b6a330a9fb1e8a52915486a027441e
Friedemann Schwenzer <Friedemann.Schwenzer@posteo.de>
2025-08-28 17:28:12 +0200

    Update library_app.py

--- library_app.py
+++ library_app.py
@@ -8,8 +8,8 @@ import elements as el
 
 # --- File paths ---
 files = {
-    2022: "data/Pankow_2022.parquet",
-    2024: "data/Pankow_2024_utf8.parquet",
+    2022: "Pankow_2022.parquet",
+    2024: "Pankow_2024_utf8.parquet",
 }
 
 # --- Page config ---
